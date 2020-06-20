c67d9c687d4824cf62ceacede81b884825b8fed1
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-20 03:21:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,17 @@ def summon_captain_planet(planeteer_calls)
   call_capt
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+check = []
+calls.collect do |count|
+ if count.length > 4
+   return true
+else check << count
+  end
+end
+  if check == calls
+    return false
+end
 end
 
 def find_the_cheese# code an argument here
